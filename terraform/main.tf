d890474899c675fca67705ceb075bb6a3c3c7b90
Marwan <marwanghoneim96@gmail.com>
2025-05-25 01:26:40 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec2" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
+  ami           = "ami-05fa00d4c63e32376"  # ✅ Updated valid AMI
   instance_type = "t2.micro"
   key_name      = "terraform"
 
